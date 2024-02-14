4d79af6581dee8244f7b292e3ea7db9cf5276424
Sarath <sarath.pr@riskcovry.com>
2024-02-13 19:56:59 -0500

    Initial Commit

--- src/app/core/services/authorization/authorization.service.ts
+++ src/app/core/services/authorization/authorization.service.ts
@@ -1,12 +1,10 @@
 import { Inject, Injectable } from '@angular/core';
-import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthorizationService {
   constructor(
-    private auth: Auth,
   ) { }
 
   signIn(params: any){
